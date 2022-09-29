94cb1cd2d124acf6ded26bf47b33d4e8e0e068ea
sadnanrakib <sadnanrakib02@gmail.com>
2022-09-29 08:41:36 +0600

    cart add

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import'./Cart.css';
 
-const Cart = () => {
+const Cart = ({cart}) => {
     return (
         <div>
-            
+            <div className='img-info'>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwaterfront&psig=AOvVaw0GaAinuXCVzguoVup1E-_V&ust=1664500010349000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJDhrPXnuPoCFQAAAAAdAAAAABAD.jpg" alt="" />
+                <div>
+                    <h6>Rakib Islam </h6>
+                    <p>Sydney, Australia</p>
+                </div>
+                </div>
+                <p>Exercise time: {cart.length}seconds</p>
         </div>
     );
 };
--- src/Component/Fitness/Fitness.js
+++ src/Component/Fitness/Fitness.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import'./Fitness.css';
 
@@ -27,14 +28,7 @@ const Fitness = () => {
                 }
             </div>
             <div className='cart-container'>
-                <div className='img-info'>
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwaterfront&psig=AOvVaw0GaAinuXCVzguoVup1E-_V&ust=1664500010349000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJDhrPXnuPoCFQAAAAAdAAAAABAD.jpg" alt="" />
-                <div>
-                    <h6>Rakib Islam</h6>
-                    <p>Sydney, Australia</p>
-                </div>
-                </div>
-                <p>Exercise time: {cart.length}seconds</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
         
